088cb15b87d6d8a6c02796e7f4fb9f24820988a9
Barenz <monthon993@gmail.com>
2018-07-24 16:24:14 +0700

    Add test suit for add friend function and create it in person.js

--- programming/person.js
+++ programming/person.js
@@ -15,4 +15,16 @@ module.exports = class Person {
         return this._name;
     }
 
+    addFriend(friendObj) {
+        if (friendObj.friends.includes(this) &&
+            this.friends.includes(friendObj)) {
+                throw new Error('We both already friends.');
+        }
+        if (this.name && friendObj.name) {
+            this.friends.push(friendObj);
+            friendObj.friends.push(this);
+        } else {
+            throw new Error('You should set your name before add a friend.');
+        }
+    }
 }
--- programming/person.test.js
+++ programming/person.test.js
@@ -11,3 +11,93 @@ it('test initialize an instance Person with a name, \
         expect(person2.name).toBe('Steve')
     }
 );
+
+it('test initailize an instance Person, \
+    name and friends attribute to be undefined', () => {
+        const person = new Person();
+        expect(person.name).toBeUndefined();
+        expect(person.friends[0]).toBeUndefined();
+    }
+);
+
+it('test call addFriend function with set a name, \
+    It should be called', () => {
+        person1 = new Person();
+        person1.name = 'Manee';
+        person1.addFriend = jest.fn();
+
+        person2 = new Person();
+        person2.name = 'Preecha';
+
+        person1.addFriend(person2);
+        expect(person1.addFriend).toHaveBeenCalledWith(person2);
+    }
+);
+
+it('test call addFriend function when not set a name, \
+    it should throw an error', () => {
+        //person1 not set a name
+        person1 = new Person();
+
+        person2 = new Person();
+        person2.name = "Manood"
+
+        expect(() => {
+            person1.addFriend(person2);
+        }).toThrow('You should set your name before add a friend.');
+
+        //person4 not set a name
+        person3 = new Person();
+        person3.name = "Meena";
+
+        person4 = new Person();
+
+        expect(() => {
+            person3.addFriend(person4);
+        }).toThrow('You should set your name before add a friend.');
+    }
+);
+
+it('test addFriend fucntion with a friend name, \
+    An attribute friend should have array of added friends.', () => {
+        person1 = new Person();
+        person2 = new Person();
+        person3 = new Person();
+
+        person1.name = 'Manee';
+        person2.name = 'Preecha';
+        person3.name = 'Premchai';
+        
+        person1.addFriend(person2);
+        expect(person1.friends).toEqual([person2]);
+
+        person1.addFriend(person3);
+        expect(person1.friends).toEqual([person2, person3])
+    }
+);
+
+it('test addFriend function repeatedly, It should throw an error.', () => {
+    person1 = new Person();
+    person2 = new Person();
+    
+    person1.name = "Sarah";
+    person2.name = "Rick";
+
+    person1.addFriend(person2);
+    expect(() => {
+        person2.addFriend(person1);
+    }).toThrow('We both already friends.');
+})
+
+it('test if A is a friend of B, B will be a friend of A too.', () => {
+    A = new Person();
+    B = new Person();
+
+    A.name = 'A';
+    B.name = 'B';
+
+    A.addFriend(B);     
+    // A <==> B
+    expect(A.friends).toEqual([B]);
+    expect(B.friends).toEqual([A]);
+});
